610ebd91bebcaee2f3a6c1f791630e98027418b4
Nouman997 <ak.aik.aik321@gmail.com>
2023-08-29 03:00:48 +0500

    working on navbar

--- codility-clone/src/app/app.module.ts
+++ codility-clone/src/app/app.module.ts
@@ -5,12 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HeaderSubMenuComponent } from './header-sub-menu/header-sub-menu.component';
+import { ListElementComponent } from './list-element/list-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeaderSubMenuComponent
+    HeaderSubMenuComponent,
+    ListElementComponent,
+
   ],
   imports: [
     BrowserModule,
